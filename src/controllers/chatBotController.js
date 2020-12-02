ac1303d43ad94e6548f087a7cfefa27c5332bf9b
Ratanaksambat19 <doung.ratanaksambat19@kit.edu.kh>
2020-12-02 16:39:05 +0700

    thunn

--- src/controllers/chatBotController.js
+++ src/controllers/chatBotController.js
@@ -131,18 +131,60 @@ function firstTrait(nlp, name) {
     }
 
     else {
-        if (entityChosen === "with$greetings"){
+        if (entityChosen === "wit$greetings"){
             callSendAPI(sender_psid, "Hii darling")
         }
 
-        if (entityChosen === "with$thanks"){
+        if (entityChosen === "wit$thanks"){
             callSendAPI(sender_psid, "You're welcome")
         }
         
-        if (entityChosen === "with$bye"){
+        if (entityChosen === "wit$bye"){
             callSendAPI(sender_psid, "Have a nice day!")
         }
     }
+
+    let response;
+      // Check if the message contains text
+    if (received_message.text) {    
+        // Create the payload for a basic text message
+        response = {
+        "text": `You sent the message: "${received_message.text}". Now send me an image!`
+        }
+    }  
+
+    else if (received_message.attachments) {
+  
+        // Gets the URL of the message attachment
+        let attachment_url = received_message.attachments[0].payload.url;
+        response = {
+            "attachment": {
+              "type": "template",
+              "payload": {
+                "template_type": "generic",
+                "elements": [{
+                  "title": "Is this the right picture?",
+                  "subtitle": "Tap a button to answer.",
+                  "image_url": attachment_url,
+                  "buttons": [
+                    {
+                      "type": "postback",
+                      "title": "Yes!",
+                      "payload": "yes",
+                    },
+                    {
+                      "type": "postback",
+                      "title": "No!",
+                      "payload": "no",
+                    }
+                  ],
+                }]
+              }
+            }
+          }
+      } 
+    // Sends the response message
+    callSendAPI(sender_psid, response);
   }
 
 // Handles messaging_postbacks events
